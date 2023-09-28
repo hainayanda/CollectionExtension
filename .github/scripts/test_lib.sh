40e15e3b9741fd25b2410f22520b76d6c0980fe1
Nayanda Haberty <hainayanda@outlook.com>
2023-09-28 22:22:31 +0700

    Fix CI

--- .github/scripts/test_lib.sh
+++ .github/scripts/test_lib.sh
@@ -2,5 +2,5 @@ set -eo pipefail
 
 xcodebuild -workspace Example/CollectionExtension.xcworkspace \
             -scheme CollectionExtension-Example \
-            -destination platform=iOS\ Simulator,OS=16.1,name=iPhone\ 14 \
+            -destination platform=iOS\ Simulator,OS=16.4,name=iPhone\ 14 \
             clean test | xcpretty
